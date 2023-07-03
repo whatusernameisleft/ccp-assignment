649519087188a3e19c8c2050a8cc5dd0dd9e1f69
whatusernameisleft <37537195+whatusernameisleft@users.noreply.github.com>
2023-07-03 10:31:47 +0800

    added minibus

--- src/io/whatusernameisleft/Minibus/Minibus.java
+++ src/io/whatusernameisleft/Minibus/Minibus.java
@@ -1,4 +1,51 @@
 package io.whatusernameisleft.Minibus;
 
-public class Minibus {
+import io.whatusernameisleft.Customer.Customer;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Minibus extends Thread {
+    private final String name;
+    private volatile boolean arrived = false;
+    private final int MAX_SEATS = 10;
+    private final BlockingQueue<Customer> seats = new ArrayBlockingQueue<>(MAX_SEATS);
+
+    public Minibus(String name) {
+        this.name = name;
+    }
+
+    public boolean board(Customer customer) {
+        return seats.offer(customer);
+    }
+
+    private void clear() {
+        seats.clear();
+    }
+
+    private void arrive() {
+        arrived = true;
+    }
+
+    private void leave() {
+        arrived = false;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                synchronized (this) {
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(10, 21) * 1000);
+                    arrive();
+                    this.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            leave();
+            clear();
+        }
+    }
 }
